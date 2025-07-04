4e1e6dfc8da02a84e5e3c191af962b4bcfb1ce07
meet-thakkar214 <meet@holbox.ai>
2025-07-04 17:15:15 +0530

    [Add Test - 1.1] Added Failing test with single number as input

--- test/StringCalculator.test.js
+++ test/StringCalculator.test.js
@@ -4,4 +4,9 @@ describe("Step 1 - Basic tests", () => {
   test("Empty string returns 0", () => {
     expect(add("")).toBe(0);
   });
+  test("Single number returns itself", () => {
+    expect(add("1")).toBe(1);
+  })
 });
+
+
