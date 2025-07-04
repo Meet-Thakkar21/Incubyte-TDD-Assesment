51b42580262b56e73d31adfba3ba39e8a9db906b
meet-thakkar214 <meet@holbox.ai>
2025-07-04 22:49:40 +0530

    [Refactor] Extracted Number Parsing Logic for clarity

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -1,19 +1,26 @@
-function add(numbers) {
-  if(numbers === "") return 0;
-  
+function parseNumbers(numbers){
   let delimiter = /,|\n/;
   let numsSection = numbers;
+  
   if(numbers.startsWith('//')){
     const endOfDelimiter = numbers.indexOf('\n');
     delimiter = new RegExp(numbers[2]);
     numsSection = numbers.slice(endOfDelimiter + 1);
-  };
+  }
+  return numsSection.split(delimiter).map(n => parseInt(n, 10));
+}
 
-  const parts = numsSection.split(delimiter).map(n => parseInt(n, 10));
+
+function add(numbers) {
+  if(numbers === "") return 0;
+
+  const parts = parseNumbers(numbers);
   const negatives = parts.filter(num => num < 0);
+
   if(negatives.length > 0) {
     throw `Negatives not allowed: ${negatives.join(',')}`
   };
+  
   return parts.reduce((acc, num) => acc + num, 0);
 }
 
